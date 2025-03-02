cf2b36cb92e24e6aaa98d9dff96f4114015d485c
Ridho Febrian <ridhofebrian.445@gmail.com>
2025-03-02 13:48:51 +0700

    membuat function store (insert data ke database)

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Post;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -15,5 +17,35 @@ class PostController extends Controller
 
         // Return data ke PostResource atau Parsing data ke PostResource
         return new PostResource( true, 'List data Posts', $posts);
+    }
+
+    public function store(Request $request)
+    {
+        // define validation rules
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        // Check if validasi fails
+        if ($validator->fails())
+        {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        // create post
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        // return response
+        return new PostResource( true, 'Data Post Berhasil Di tambahkan', $post);
     }
 }
